12614f2da588307f7d4521566b76ebe01a301f2b
Jason Twichell <jtwichell@core-techs.net>
2019-10-02 10:36:15 -0400

    Initial commit

---
+++ Scripts/Custom.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $('.customModal').click(function () {
+        var buttonId = $(this).attr('id');
+        $('#modal-container').removeAttr('class').addClass(buttonId);
+        $('body').addClass('modal-active');
+    });
+
+    $('#modal-container').click(function () {
+        $(this).addClass('out');
+        $('body').removeClass('modal-active');
+    });
+});
